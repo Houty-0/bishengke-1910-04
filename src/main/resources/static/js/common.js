3326d00fb2da96fa3a005e237f02772f8a7fd248
Houty-0 <tyhou001@outlook.com>
2020-02-16 10:53:37 +0800

    商品列表页面增加了购物盒部分

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -6,4 +6,74 @@ $(function() {
 		$(".ui-dialog").show();
 		$(".scanQrcode").show();
 	});
+
+	/*
+	 * function doLogin(){ $("#j-dialog").load("/login/login.html");
+	 * $(".ui-dialog-overlay").show(); $(".ui-dialog").show();
+	 * $(".scanQrcode").show(); }
+	 */
 })
+
+function doHandleResult(data) {
+	var products = "";
+	$(data).each(
+			function(index, good) {
+				var baoZ = good.baoZhuang;
+				if (baoZ == null) {
+					baoZ = "";
+				} else {
+					baoZ = "/" + baoZ;
+				}
+				products += '<div class="product" value="' + good.note + '">'
+						+ '<div class="img cursor" style="height: 139px;">'
+						+ '<img src="' + good.imgUrl + '">' + '</div>'
+						+ '<div class="title">' + good.name + '</div>'
+						+ '<div class="desc grey">'
+						+ '<div class="menu-heat-desc menu-heat-desc0"></div>'
+						+ '</div><div class="price red">'
+						+ '<span class="font16 font-weight">' + good.price
+						+ '</span>元' + '<span style="color: #7f7f7f;">' + baoZ
+						+ '</span>' + '</div>'
+						+ '<div class="order j-menu-order">'
+						+ '<div class="start ui-bgbtn-green" onclick="toOrderDetail()">开始订餐</div>'
+						+ '</div>' + '</div>';
+
+			});
+	return products;
+	
+}
+
+//点击菜单项查询对应菜单商品列表
+function doFindObjects(menuId) {
+	//var _menuId = $("#j-menu-left").data("menuId");
+	// $(".m-menu-content.right").empty();
+	//var menuId = $(this).prop("id");
+	/* if(menuId==null){
+		menuId = _menuId;
+	} */
+	//debugger
+	$("#j-menu-left").data("menuId",menuId);// 保存当前展示的是哪个菜单Id对应的商品列表
+	var url = "/goods/doFindObjects";
+	var params = {
+		"menuId" : menuId
+	};
+
+
+	if (menuId != 1) {
+		$(".m-condition").addClass("hidden");
+		$(".m-product-list").css("margin-top", 0);
+	} else {
+		$(".m-condition").removeClass("hidden");
+		$(".m-product-list").css("margin-top", 88);
+	}
+
+	$.get(url, params, function(result) {
+		if (result.state == 1) {
+			if ($(".m-product-list").empty()) {
+				var products = doHandleResult(result.data);
+				$(".m-product-list").append(products);
+			}
+		}
+	});
+
+}
--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -29,23 +29,60 @@ $(function() {
 		tap.prop("class", "tap on");
 		tap.siblings().prop("class", "tap");
 	});
-	
+
 	/* 商品详细介绍弹出层关闭 */
-	$(".m-pop-detail .close").click(function(){
+	$(".m-pop-detail .close").click(function() {
 		$(".ui-dialog-overlay").hide();
 		$(".m-pop-detail").hide();
 	});
-	
-	
-	/*$(".m-main").waypoint(function(){
-		$(".m-main .m-menu ul").removeAttr("style","top");
-		$(".m-main .m-menu ul").css({"position":"static","bottom":0});
-		},{ offset: '-60%'});
-		
-	$(".m-main").waypoint(function(){
-		$(".m-main .m-menu ul").removeAttr("style","bottom");
-		$(".m-main .m-menu ul").css({"position":"fixed","top":"80px"});
-		},{ offset: -200});*/
-	
-	
+
+	/*
+	 * $(".m-main").waypoint(function(){ $(".m-main .m-menu
+	 * ul").removeAttr("style","top"); $(".m-main .m-menu
+	 * ul").css({"position":"static","bottom":0}); },{ offset: '-60%'});
+	 * 
+	 * $(".m-main").waypoint(function(){ $(".m-main .m-menu
+	 * ul").removeAttr("style","bottom"); $(".m-main .m-menu
+	 * ul").css({"position":"fixed","top":"80px"}); },{ offset: -200});
+	 */
+
 });
+
+/*$(function() {
+	 弹出商品详细介绍层 
+	$(".m-product-list").on("click", ".img", popupProductDesc);
+
+})*/
+
+function popupProductDesc() {
+	$(".ui-dialog-overlay").show();
+	/* 获取商品描述详细信息 */
+	var idesccn = $(this).parent().attr("value");
+	/* 获取图片src地址 */
+	var imgSrc = $(this).children().attr("src");
+	/* 获取商品的名称 */
+	var title = $(this).siblings(".title").html();
+	/* 获取商品价格和包装类型 */
+	var price = $(this).siblings(".price").children().eq(0).html();
+	var baoZ = $(this).siblings(".price").children().eq(1).html();
+	if (baoZ == null)
+		baoZ = "";
+
+	/* 給弹出层的商品添加详细信息内容 */
+	$(".m-pop-detail .right-content .menu-desc").html(idesccn);
+	/* 給弹出层的商品添加图片地址 */
+	$(".m-pop-detail .left-content").children().attr("src", imgSrc);
+	/* 給弹出层的商品添加名称 */
+	$(".m-pop-detail .right-content .menu-info .name").html(title);
+	/* 給弹出层的商品添加价格和包装类型 */
+
+	$(".m-pop-detail .right-content .price-info .price").html(
+			price + "元" + baoZ);
+
+	$(".m-pop-detail").show();
+}
+
+/* 选规格加载订单详情 */
+function toOrderDetail() {
+	$(".m-menu-content.right").load("other/orderDetail.html");
+}
